644cc95dead4333c1456ffa297cfa931c3168315
Kostas Milonas <milonas.ko@gmail.com>
2019-08-20 12:00:02 +0300

    adds extra acars frequency to capture more signals

--- acarsserver/config/settings.py
+++ acarsserver/config/settings.py
@@ -1,6 +1,6 @@
 web_host = '0.0.0.0'
 web_port = 8080
 
-acars_frequencies = ['131.550', '131.525', '131.725', '131.850']
+acars_frequencies = ['131.550', '131.525', '131.725', '131.825', '131.850']
 
 queue_name_image_download = 'image_download'
